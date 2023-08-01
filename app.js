2df764656ff293cc89ce5f50ebc7b0ecae0c89a9
Tomas Aguero <tomi.aguero123@gmail.com>
2023-08-01 18:50:15 -0300

    Segunda preentrega

--- app.js
+++ app.js
@@ -1,28 +1,63 @@
-// Verificar si un número es par o impar
-let numero = parseInt(prompt("Ingrese un número para saber si es par o impar:"));
 
-if (numero % 2 === 0) {
-    alert("El número es par");
-} else {
-    alert("El número es impar");
+// capturar entradas mediante prompt()
+const num1 = Number(prompt("Ingresa el primer número:"));
+const num2 = Number(prompt("Ingresa el segundo número:"));
+
+// declarar variables y objetos necesarios
+const datos = {
+  numeros: [num1, num2],
+};
+
+// crear funciones para realizar operaciones
+function suma(a, b) {
+  return a + b;
 }
 
-
-// Tabla de multiplicar
-let m_numero = parseInt(prompt("Ingrese un número para mostrar su tabla de multiplicacion!:"));
-
-console.log("Tabla de multiplicar del " + m_numero + ":");
-
-for (let i = 1; i <= 10; i++) {
-    let resultado = m_numero * i;
-    console.log(m_numero + " x " + i + " = " + resultado);
+function resta(a, b) {
+  return a - b;
 }
 
+function division(a, b) {
+  if (b !== 0) {
+    return a / b;
+  } else {
+    return "Error: No se puede dividir por cero.";
+  }
+}
 
+function porcentaje(a, b) {
+  return (a * b) / 100;
+}
 
-// Calculadora de suma
-let numero1 = parseFloat(prompt("Ingrese el primer número de la suma:"));
-let numero2 = parseFloat(prompt("Ingrese el segundo número de la suma:"));
+// realizar operaciones
+const resultadoSuma = suma(num1, num2);
+const resultadoResta = resta(num1, num2);
+const resultadoDivision = division(num1, num2);
+const resultadoPorcentaje = porcentaje(num1, num2);
 
-let suma = numero1 + numero2;
-alert("El resultado de la suma es: " + suma);
+// realizar una salida (resultados)
+console.log("Resultados:");
+console.log("Suma:", resultadoSuma);
+console.log("Resta:", resultadoResta);
+console.log("División:", resultadoDivision);
+console.log("Porcentaje:", resultadoPorcentaje);
+
+// arrays, métodos de búsqueda y filtrado sobre el Array
+const arrayNumeros = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+// metodo de busqueda
+function buscarNumero(numero) {
+  return arrayNumeros.includes(numero);
+}
+
+// metodo de filtrado
+function filtrarNumerosMayoresA(valor) {
+  return arrayNumeros.filter(num => num > valor);
+}
+
+// uso de los metodos de busqueda y filtrado
+const numeroBuscado = 50;
+console.log("El número", numeroBuscado, "se encuentra en el array:", buscarNumero(numeroBuscado));
+
+const valorFiltro = 35;
+console.log("Números mayores a", valorFiltro, ":", filtrarNumerosMayoresA(valorFiltro));
